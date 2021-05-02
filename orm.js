820d66e49f887bc93f9d2e34a30e1ed47f2c0406
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-02 19:55:19 -0400

    Updated the orm file

--- orm.js
+++ orm.js
@@ -4,3 +4,42 @@ const chalk = require('chalk');
  
 const queryAsync = util.promisify(connection.query).bind(connection);
 const orm = {
+    //Record Select
+    selectAll: async function (table) {
+        try {
+                const queryString = "select * from ??";
+                const result = await queryAsync(queryString, [table]);
+                return (result);
+        }
+        catch
+        {
+                console.log(chalk.yellowBright.black("error"));
+        }
+}, 
+//Record insert
+insertOne: async function (table, oneValue, twoValue) {
+    try {
+            const queryString = "insert into ??(burger_name,devoured)values(?,?)";
+            const result = await queryAsync(queryString, [table, oneValue, twoValue]);
+            return (result);
+    }
+    catch
+    {
+            console.log(chalk.yellowBright.black("error"));
+    }
+},
+
+//Record Update
+updateOne: async function (table, whereValue, idValue) {
+    try {
+            const queryString = "update ?? set devoured=? where id=?";
+            const result = await queryAsync(queryString, [table, whereValue, idValue]);
+            return (result);
+    }
+    catch
+    {
+            console.log(chalk.yellowBright.black("error"));
+    }
+}
+};
+module.exports = orm;
