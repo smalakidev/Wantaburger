8d43de24e243266945a2d3ed27502f092e36994c
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-02 19:36:38 -0400

    Added more code to the connection.js and connected to both the local host and Heroku

--- connection.js
+++ connection.js
@@ -2,8 +2,26 @@ const mysql = require('mysql');
 const chalk = require('chalk');
 require("dotenv").config();
 let connection;
-//Heroku connection
+//This is the Heroku Connection
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
+//This is the connection to local host
+connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB
+  });
+}
+connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log(chalk.bgGreenBright.black("connected as id " + connection.threadId));
+  });
+  
+  module.exports = connection;
