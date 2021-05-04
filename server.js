c2c3e57f48f6b46f1b9c104855c684992f263043
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-04 16:29:55 -0400

    made edits to server.js and also to file names

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./Controllers/burgers_controller");
+const routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
