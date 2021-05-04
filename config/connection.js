f9217f1a42082e755ef7d7a70553540df2b6802c
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-04 11:35:24 -0400

    Made edits to package.json

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,11 @@ if (process.env.JAWSDB_URL) {
 else {
 //This is the connection to local host
 connection = mysql.createConnection({
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || "3306",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "Password69",
-    database: process.env.DB || "burgers_db"
+    host: process.env.DB_HOST, 
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
   });
 }
 connection.connect(function (err) {
